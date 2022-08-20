b9a8fd175cb2386c4ecab3df6d5b6528e04472a1
jungeuno <jungeuno76@gmail.com>
2022-08-20 16:25:32 +0900

    REST API
    RESTful API 활용하여 웝 브라우징과 같은 기능 생성

--- lab_server/02. REST_API.py
+++ lab_server/02. REST_API.py
@@ -1,18 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request                  # jsonify - json 형식으로 변환해줌
 app = Flask(__name__)
 
 @app.route('/api1', methods=['GET'])                       # 기본값은 Get 방식
 def f1():
-    return 'GET api1'
+    return jsonify({'status': 'success'})                  # 쌍따옴표로 변환되어 확인됨
 
 
-
-
-
-@app.route('/api2', methods=['POST'])
+@app.route('/api2', methods=['POST'])                      # POST 방식
 def f2():
-    return 'POST api2'
-
+    data = request.get.json()
+    return jsonify(data)
 
 
 app.run()
