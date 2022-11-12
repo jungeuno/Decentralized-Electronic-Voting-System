c7377462dc70ad12462758933e8e76c18aeb99b8
jungeuno <jungeuno76@gmail.com>
2022-11-12 17:28:10 +0900

    전자 서명

--- lab_blockchain/06. digital_signature.py
+++ lab_blockchain/06. digital_signature.py
@@ -1,6 +1,6 @@
 from ecdsa import SigningKey
 import hashlib
-
+# 전자 서명 - 키 값을 비교하여 위조를 확인함
 private_key = SigningKey.generate()
 public_key = private_key.get_verifying_key()
 wallet_address = hashlib.sha256(public_key.to_string()).hexdigest()
